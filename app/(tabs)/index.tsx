f3224961c9df5390622baeadfa9293e8169cac3b
Rodrigo <rodrigodelgadoo.005@gmail.com>
2024-11-02 22:35:57 -0300

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,106 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { StyleSheet, TouchableOpacity, View, Text, ImageBackground, Image, Dimensions, SafeAreaView } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { useNavigation } from '@react-navigation/native';
+
+const { width, height } = Dimensions.get('window');
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    navigation.navigate('Login');
+  };
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.safeArea}>
+      <ImageBackground
+        source={require('/RN4/rn4/assets/images/background.jpg')}
+        style={styles.background}
+        resizeMode="cover"
+      >
+        <View style={styles.container}>
+          <View style={styles.contentContainer}>
+            <View style={styles.logoTamaño}>
+              <Image
+                source={require('/RN4/rn4/assets/images/copa.png')}
+                style={styles.logo}
+                resizeMode="contain"
+              />
+            </View>
+            <ThemedText type="title" style={styles.title}>
+              ESTANGA <Text style={styles.estangaText}>LEAGUE</Text>
+            </ThemedText>
+            <ThemedText style={styles.text}>
+              Disfruta de la Mejor Liga del Fútbol Mundial dentro del Terreno de Juego Mas Prestigioso.
+            </ThemedText>
+            <TouchableOpacity style={styles.button} onPress={handlePress}>
+              <Text style={styles.buttonText}>COMENZAR</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  safeArea: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    width: '100%',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(0,15,44,0.7)',
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
+    paddingHorizontal: 20,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  logoTamaño: {
+    width: 150,
+    height:175,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 19,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  logo: {
+    width: '100%',
+    height: '100%',
+  },
+  title: {
+    fontSize: 33,
+    fontWeight: '',
+    color: 'white',
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  estangaText: {
+    color: '#00B7ED',
+  },
+  text: {
+    color: 'white',
+    fontSize: 17,
+    textAlign: 'center',
+    marginBottom: 32,
+    paddingHorizontal: 10,
+    lineHeight: 24,
+  },
+  button: {
+    backgroundColor: '#00B7ED',
+    paddingVertical: 15,
+    borderRadius: 25,
+    width: '70%',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 19,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    letterSpacing: 2,
   },
 });
